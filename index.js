3cd0b0e22bbd71fb8bede504b8b9ce60dbdc0336
Jacob Kelley <jakie8@gmail.com>
2015-05-11 13:49:12 -0700

    Spaces to tabs again :(

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ module.exports =
 	"Cache": require("./lib/cache"),
 	"CacheProviders":
 	{
-    "Disk": require("./lib/cache_providers/disk"),
-    "Memory": require("./lib/cache_providers/memory"),
+		"Disk": require("./lib/cache_providers/disk"),
+		"Memory": require("./lib/cache_providers/memory"),
 		"Redis": require("./lib/cache_providers/redis")
 	}
 };
